f81e37feb50585c581ba4a647163a15071c162b3
Nguyen Tu <tu.lqd.163@gmail.com>
2021-03-03 14:25:07 +0700

    push

---
+++ create-react-app-antd-master/src/data.source.js
@@ -0,0 +1,498 @@
+import React from 'react';
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page klsyipud5r9-editor_css' },
+  logo: {
+    className: 'header0-logo klsykawpfh7-editor_css',
+    children:
+      'https://vhub.vanlanguni.edu.vn/Storages/Logos/_hjkj3598HJ_logo2.png',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <p>Home</p>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+          {
+            name: 'sub1',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: 'item2',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <span>
+                    <p>About Us</p>
+                  </span>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+};
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title',
+    children: 'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+  },
+  content: {
+    className: 'banner0-content',
+    children: '一个高效的页面动画解决方案',
+  },
+  button: { className: 'banner0-button', children: 'Learn More' },
+};
+export const Feature00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [{ name: 'title', children: '产品与服务' }],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: '一站式业务接入',
+            },
+            { name: 'content', children: '支付、结算、核算接入产品效率翻四倍' },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: '一站式事中风险监控',
+            },
+            {
+              name: 'content',
+              children: '在所有需求配置环节事前风险控制和质量控制能力',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: '一站式数据运营',
+            },
+            {
+              name: 'content',
+              children: '沉淀产品接入效率和运营小二工作效率数据',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Feature10DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: { className: 'home-page content1', playScale: 0.3 },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/nLzbeGQLPyBJoli.png',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: { className: 'content1-title', children: '企业资源管理' },
+  content: {
+    className: 'content1-content',
+    children:
+      '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。',
+  },
+};
+export const Feature50DataSource = {
+  wrapper: { className: 'home-page-wrapper content7-wrapper' },
+  page: { className: 'home-page content7' },
+  OverPack: {},
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: '蚂蚁金融云提供专业的服务',
+        className: 'title-h1',
+      },
+      { name: 'content', children: '基于阿里云计算强大的基础资源' },
+    ],
+  },
+  tabsWrapper: { className: 'content7-tabs-wrapper' },
+  block: {
+    children: [
+      {
+        name: 'block0',
+        tag: {
+          className: 'content7-tag',
+          text: { children: 'PHONE', className: 'content7-tag-name' },
+          icon: { children: 'mobile' },
+        },
+        content: {
+          className: 'content7-content',
+          text: {
+            className: 'content7-text',
+            md: 14,
+            xs: 24,
+            children: (
+              <span>
+                <h3>技术</h3>
+                <p>
+                  丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
+                </p>
+                <br />
+                <h3>融合</h3>
+                <p>
+                  解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
+                </p>
+                <br />
+                <h3>
+                  开放
+                </h3>符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。
+              </span>
+            ),
+          },
+          img: {
+            className: 'content7-img',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png',
+            md: 10,
+            xs: 24,
+          },
+        },
+      },
+      {
+        name: 'block1',
+        tag: {
+          className: 'content7-tag',
+          icon: { children: 'tablet' },
+          text: { className: 'content7-tag-name', children: 'TABLET' },
+        },
+        content: {
+          className: 'content7-content',
+          text: {
+            className: 'content7-text',
+            md: 14,
+            xs: 24,
+            children: (
+              <span>
+                <h3>技术</h3>
+                <p>
+                  丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
+                </p>
+                <br />
+                <h3>融合</h3>
+                <p>
+                  解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
+                </p>
+                <br />
+                <h3>
+                  开放
+                </h3>符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。
+              </span>
+            ),
+          },
+          img: {
+            className: 'content7-img',
+            md: 10,
+            xs: 24,
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png',
+          },
+        },
+      },
+      {
+        name: 'block2',
+        tag: {
+          className: 'content7-tag',
+          text: { children: 'DESKTOP', className: 'content7-tag-name' },
+          icon: { children: 'laptop' },
+        },
+        content: {
+          className: 'content7-content',
+          text: {
+            className: 'content7-text',
+            md: 14,
+            xs: 24,
+            children: (
+              <span>
+                <h3>技术</h3>
+                <p>
+                  丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
+                </p>
+                <br />
+                <h3>融合</h3>
+                <p>
+                  解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
+                </p>
+                <br />
+                <h3>
+                  开放
+                </h3>符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。
+              </span>
+            ),
+          },
+          img: {
+            className: 'content7-img',
+            md: 10,
+            xs: 24,
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png',
+          },
+        },
+      },
+    ],
+    className: 'klt05vkpm4q-editor_css',
+  },
+};
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    gutter: 0,
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children: 'https://www.vanlanguni.edu.vn/images/logos/logo-vlu.png',
+        },
+        childWrapper: {
+          className: 'slogan',
+          children: [
+            {
+              name: 'content0',
+              children: (
+                <span>
+                  <span>
+                    <p>
+                      <br />
+                    </p>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        xs: 24,
+        md: 10,
+        className: 'block',
+        title: {
+          children: (
+            <span>
+              <span>
+                <p>ĐẠI HỌC VĂN LANG</p>
+              </span>
+            </span>
+          ),
+          className: 'klsyng1mdk8-editor_css',
+        },
+        childWrapper: {
+          children: [
+            {
+              name: 'link0',
+              href: '#',
+              children: (
+                <span>
+                  <span>
+                    <p>
+                      <b>
+                        Trụ sở: 45 Nguyễn Khắc Nhu, P. Cô Giang, Q.1, TP. HCM
+                      </b>
+                    </p>
+                  </span>
+                </span>
+              ),
+              className: 'klsynfpfm1-editor_css',
+            },
+            {
+              name: 'link1',
+              href: '#',
+              children: (
+                <span>
+                  <span>
+                    <p>
+                      <b>
+                        Cơ sở 2: 233A Phan Văn Trị, P.11, Q. Bình Thạnh, TP. HCM
+                      </b>
+                    </p>
+                  </span>
+                </span>
+              ),
+              className: 'klsyprde0bp-editor_css',
+            },
+            {
+              name: 'link2',
+              href: '#',
+              children: (
+                <span>
+                  <span>
+                    <p>
+                      <b>
+                        Cơ sở 3: 69/68 Đặng Thùy Trâm, P. 13, Q. Bình Thạnh, TP.
+                        HCM
+                      </b>
+                    </p>
+                  </span>
+                </span>
+              ),
+              className: 'klsyqg69ap-editor_css',
+            },
+            {
+              name: 'link3',
+              href: '#',
+              children: (
+                <span>
+                  <span>
+                    <p>
+                      <b>
+                        Ký túc xá: 160/63A-B Phan Huy Ích, P. 12, Q. Gò Vấp, TP.
+                        HCM
+                      </b>
+                    </p>
+                  </span>
+                </span>
+              ),
+              className: 'klsyqpnr9vn-editor_css',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <span>
+          <span>VAN LANG LEGEND - TEAM 12</span>
+        </span>
+      </span>
+    ),
+  },
+};
